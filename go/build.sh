f74df79731aa5c3c8da319ea651e0c30a7a6387f
Benjamin Ang <ben.ang@fastmail.com>
2022-11-24 21:13:23 -0500

    Adjust build script for convenience

--- go/build.sh
+++ go/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Usage: build.sh <challenge directory>
+# Usage: build.sh <challenge directory> <executable_name>
 cd "$1" || exit 1
 # CGO will sometimes use dynamic linking and introduce dependencies on shared
 # libraries, but here I want static binaries that I can just scp to a server
 # and run.
-CGO_ENABLED=0 GOOS=linux go build -a -o "$1"
+CGO_ENABLED=0 GOOS=linux go build -a -o "$2"
