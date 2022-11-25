2149f708aa6a78839fd944eb877f8213eebf083f
Benjamin Ang <ben.ang@fastmail.com>
2022-11-25 00:07:09 -0500

    BudgetChat: add function to validate names

--- go/3_budget_chat/budget_chat.go
+++ go/3_budget_chat/budget_chat.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 )
 
-const welcomeString = "You've connected to budget chat. Please input a username to use for this session: \n"
+const (
+	welcomeString    = "You've connected to budget chat. Please input a username to use for this session.\n"
+	nameErrorMessage = "Usernames must contain only alphanumeric characters and cannot be longer than 16 characters in length.\n"
+)
+
+var (
+	alphanumericRegexp = regexp.MustCompile(`^[a-zA-Z0-9]*$`)
+	ErrEmptyName       = errors.New("Name cannot be an empty string")
+	ErrInvalidName     = errors.New("Name can only contain alphanumeric characters")
+	ErrNameTooLong     = errors.New("Name cannot be longer than 16 characters")
+)
 
 func main() {
 	s := StartServer()
@@ -41,6 +54,29 @@ func handle(conn net.Conn) {
 	b := bufio.NewReader(conn)
 	conn.Write([]byte(welcomeString))
 	log.Println("Sent welcome message to", conn.RemoteAddr())
+
+	name, e := b.ReadBytes('\n')
+	if e != nil {
+		log.Printf("Error reading username from %v, %v", conn.RemoteAddr(), e)
+		return
+	}
+	e = validateName(string(name))
+	log.Printf("Recieved name %s", string(name))
+
+	switch e {
+	case ErrEmptyName:
+		conn.Write([]byte("You need a username to continue.\n"))
+		return
+	case ErrInvalidName:
+		conn.Write([]byte(nameErrorMessage))
+		return
+	case ErrNameTooLong:
+		conn.Write([]byte(nameErrorMessage))
+		return
+	case nil:
+		log.Printf("Registering client %v, as %s!", conn.RemoteAddr(), strings.TrimRight(string(name), "\n"))
+	}
+
 	for {
 		_, e := b.ReadBytes('\n')
 		if e != nil {
@@ -48,4 +84,18 @@ func handle(conn net.Conn) {
 		}
 	}
 	log.Println("closed connection from", conn.RemoteAddr())
+}
+
+func validateName(name string) error {
+	name = strings.TrimRight(name, "\n")
+	if name == "" {
+		return ErrEmptyName
+	}
+	if len(name) > 16 {
+		return ErrNameTooLong
+	}
+	if !alphanumericRegexp.Match([]byte(name)) {
+		return ErrInvalidName
+	}
+	return nil
 }
--- go/3_budget_chat/budget_chat_test.go
+++ go/3_budget_chat/budget_chat_test.go
@@ -21,8 +21,49 @@ func TestBudgetChat(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error reading from connection: %v", err)
 	}
-
 	if string(got) != welcomeString {
 		t.Errorf("wanted welcome message '%s', got '%s'", welcomeString, got)
+	}
+
+func TestValidateName(t *testing.T) {
+	tt := []struct {
+		test string
+		name string
+		want error
+	}{
+		{
+			"accepts a name containing letters",
+			"Alice\n",
+			nil,
+		},
+		{
+			"accepts a name containing alphanumeric characters",
+			"Bob123\n",
+			nil,
+		},
+		{
+			"rejects names containing non-alphanumeric characters",
+			"Chuck!\n",
+			ErrInvalidName,
+		},
+		{
+			"rejects names which are just empty strings",
+			"\n",
+			ErrEmptyName,
+		},
+		{
+			"rejects names longer than 16 characters",
+			"12345678901234567\n",
+			ErrNameTooLong,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.test, func(t *testing.T) {
+			got := validateName(tc.name)
+			if got != tc.want {
+				t.Errorf("expected err '%v', got '%v'", tc.want, got)
+			}
+		})
 	}
 }
