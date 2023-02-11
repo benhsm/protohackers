996febba02d05d17ab6d9fe41c29c1a21c8b272e
Benjamin Ang <ben.ang@fastmail.com>
2023-02-11 10:10:21 -0500

    smoke_test: finished rust implementation
    This implementation passes the test. It uses manual copying.

--- rust/00_smoke_test/src/main.rs
+++ rust/00_smoke_test/src/main.rs
@@ -1,18 +1,43 @@
-use core::time;
-
-use tokio::{net::{TcpListener, TcpStream}, io::{AsyncReadExt, AsyncWriteExt}};
+use log::{ info, error, debug };
+use tokio::{net::TcpListener, io::{self, AsyncReadExt, AsyncWriteExt}};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ln = TcpListener::bind("0.0.0.0:1337").await?;
+async fn main() -> io::Result<()> {
+    env_logger::init();
+    let port = 1337;
+    let ln = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+    info!("started TCP listener at port {}", port);
     
     loop {
-        let (mut conn, _addr) = ln.accept().await?;
+        let (mut conn, addr) = ln.accept().await?;
+        info!("accepting new connection from: {}", addr);
 
         tokio::spawn(async move {
             let mut buf  = [0; 1024];
-            conn.read(&mut buf).await;
-            conn.write_all(&mut buf).await;
+            loop {
+                match conn.read(&mut buf).await {
+                    Ok(0) => {
+                        info!("recieved EOF from: {}", addr);
+                        return;
+                    },
+                    Ok(n) => {
+                        if let Err(err) = conn.write_all(&mut buf[..n]).await {
+                            error!("unexpected error writing to connection {:?}: {}",
+                                   addr,
+                                   err);
+                            return;
+                        }
+                        info!("wrote {} bytes to {}", n, addr); 
+                        debug!("content of message to {}: {:?}", addr, &buf[..n]);
+                    },
+                    Err(err) => {
+                        error!("unexpected error on connection {}: {}",
+                               addr,
+                               err);
+                        return;
+                    }
+                }
+            }
         });
     }
 }
